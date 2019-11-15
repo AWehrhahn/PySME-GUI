dabdf01d0df21ed64283d3e1235e14d7f112fa90
Ansgar Wehrhahn <ansgar.wehrhahn@physics.uu.se>
2019-11-15 18:07:03 +0100

    Better Parameter support

--- src/js/parameters.js
+++ src/js/parameters.js
@@ -1,15 +1,28 @@
 
-const update_parameters = () => {
-    get_parameters((err, res) => {
-        console.log(res)
-        var teff = document.getElementById("param-teff")
-        teff.value = res.teff.toFixed(2);
 
+var teff = document.getElementById("parameter-teff")
+var logg = document.getElementById("parameter-logg")
+var monh = document.getElementById("parameter-monh")
+
+const update_parameters = () => {
+    get_parameters().then((res) => {
+        teff.value = res.teff.toFixed(2);
+        logg.value = res.logg.toFixed(2);
+        monh.value = res.monh.toFixed(2);
     })
 }
 
-$(".parameter").on("input", () => {
-    console.log("Teff changed");
-    $(this).css("has-error", "true")
-    console.log($(this))
+$(".parameter").on("input", function () {
+    var content = Number(this.value);
+    if (Number.isNaN(content)) {
+        $(this).css("border-color", "red");
+    } else {
+        $(this).css("border-color", "green")
+
+        let name = this.id.split("-")[1]
+        let dict = {};
+        dict[name] = content;
+        set_parameters(dict);
+
+    }
 })
