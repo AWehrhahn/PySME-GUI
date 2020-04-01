0d774f1f6557214828ca22704d413e80aad9f7ee
Ansgar Wehrhahn <ansgar.wehrhahn@physics.uu.se>
2020-04-01 16:58:45 +0200

    New GUI

---
+++ src/plotting.ts
@@ -0,0 +1,274 @@
+var Plotly = require("plotly.js")
+// import * as Plotly from 'plotly.js';
+
+// Define the colors and linestyles to use in the plot
+var fmt = {
+    Obs: { color: "#1f77b4", linestyle: "solid" },
+    Syn: { color: "#ff7f0e", linestyle: "solid", marker: "" },
+    LineMask: { facecolor: "#bcbd22", alpha: 1 },
+    ContMask: { facecolor: "#d62728", alpha: 1 },
+}
+
+var mask_mode: "bad" | "line" | "cont" = "cont"
+
+var graphDiv: any = document.getElementById('graphDiv');
+var data: any = []
+var layout: any = {}
+var data_index: { [id: string]: { [id: number]: number } } = {
+    linemask: {},
+    contmask: {},
+    spec: {},
+    synth: {}
+}
+
+function get_mask_value() {
+    if (mask_mode == "bad") return 0
+    if (mask_mode == "line") return 1
+    if (mask_mode == "cont") return 2
+}
+
+function get_segment() {
+    var seg = layout.sliders[0].active
+    return seg
+}
+
+function get_data_index(seg: number, type: "linemask" | "contmask" | "spec" | "synth") {
+    return data_index[type][seg]
+}
+
+function set_mask(sme: SmeFile, range: [number, number]) {
+    var value = get_mask_value()
+    var seg = get_segment()
+
+    var wave: Float64Array = sme.wave[seg]
+    var wmin = range[0]
+    var wmax = range[1]
+
+    if (typeof (sme.mask[seg][0]) === "bigint") {
+        var mask: BigInt64Array = sme.mask[seg]
+        var big_value = BigInt(value)
+        sme.mask[seg] = mask.map((v, i) => {
+            if ((wave[i] >= wmin) && (wave[i] <= wmax)) {
+                return big_value
+            } else {
+                return v
+            }
+        })
+    } else {
+        var mask2: Uint8Array = sme.mask[seg]
+        sme.mask[seg] = mask2.map((v, i) => {
+            if ((wave[i] >= wmin) && (wave[i] <= wmax)) {
+                return value
+            } else {
+                return v
+            }
+        })
+    }
+
+    var idx = get_data_index(seg, "linemask")
+    var [x, y] = create_mask_points(
+        sme.wave[seg], sme.spec[seg], sme.mask[seg], 1
+    )
+    data[idx].x = x
+    data[idx].y = y
+
+    var idx = get_data_index(seg, "contmask")
+    var [x, y] = create_mask_points(
+        sme.wave[seg], sme.spec[seg], sme.mask[seg], 2
+    )
+
+    data[idx].x = x
+    data[idx].y = y
+
+    layout.datarevision = layout.datarevision + 1
+    Plotly.react(graphDiv, data, layout)
+}
+
+function create_mask_points(x: Float64Array, y: Float64Array, mask: Uint8Array, value: number) {
+    // Creates the points that define the outer edge of the mask region
+    y = y.map((v, i) => {
+        if (mask[i] == value) {
+            return v
+        } else {
+            return 0
+        }
+    })
+
+    x = x.map((v, i, a) => {
+        if ((i == 0) || (i == a.length - 1)) {
+            return v
+        } else {
+            if ((mask[i] != value) && (mask[i + 1] == value)) {
+                return a[i + 1]
+            } else {
+                if ((mask[i] != value) && (mask[i - 1] == value)) {
+                    return a[i - 1]
+                } else {
+                    return v
+                }
+            }
+        }
+    })
+
+    return [x, y]
+}
+
+function plot_sme(sme: any) {
+    var nSegment = sme["wave"].length
+
+    // Create Plot
+    var annotations: any;
+    var visible: any = [];
+    var line_mask_idx: { [id: number]: any } = {};
+    var cont_mask_idx: { [id: number]: any } = {};
+    var steps = []
+    var counter = 0;
+
+
+    for (let seg = 0; seg < nSegment; seg++) {
+        var k = visible.length
+        line_mask_idx[seg] = k
+        cont_mask_idx[seg] = k + 1
+        // The order of the plots is chosen by the z order, from low to high
+        // Masks should be below the spectra (so they don't hide half of the line)
+        // Synthetic on top of observation, because synthetic varies less than observation
+        // Annoying I know, but plotly doesn't seem to have good controls for the z order
+        // Or Legend order for that matter
+
+        if (sme.mask) {
+            // Line mask
+            var [x, y] = create_mask_points(
+                sme.wave[seg], sme.spec[seg], sme.mask[seg], 1
+            )
+
+            data.push({
+                x: x,
+                y: y,
+                fillcolor: fmt["LineMask"]["facecolor"],
+                fill: "tozeroy",
+                mode: "none",
+                name: "Line Mask",
+                hoverinfo: "none",
+                legendgroup: 2,
+                visible: 0 == seg,
+            })
+            visible.push(seg)
+            data_index["linemask"][seg] = counter
+            counter += 1
+
+            // Cont mask
+            var [x, y] = create_mask_points(
+                sme.wave[seg], sme.spec[seg], sme.mask[seg], 2
+            )
+
+            data.push(
+                {
+                    x: x,
+                    y: y,
+                    fillcolor: fmt["ContMask"]["facecolor"],
+                    fill: "tozeroy",
+                    mode: "none",
+                    name: "Continuum Mask",
+                    hoverinfo: "none",
+                    legendgroup: 2,
+                    visible: 0 == seg,
+                }
+            )
+            visible.push(seg)
+            data_index["contmask"][seg] = counter
+            counter += 1
+        }
+
+        if (sme.spec) {
+            //Observation
+            data.push({
+                x: sme.wave[seg],
+                y: sme.spec[seg],
+                line: { "color": fmt["Obs"]["color"] },
+                name: "Observation",
+                legendgroup: 0,
+                visible: 0 == seg,
+            })
+            visible.push(seg)
+            data_index["spec"][seg] = counter
+            counter += 1
+        }
+        if (sme.synth) {
+            // Synthetic
+            data.push({
+                x: sme.wave[seg],
+                y: sme.synth[seg],
+                name: "Synthethic",
+                line: { "color": fmt["Syn"]["color"] },
+                legendgroup: 1,
+                visible: 0 == seg,
+            })
+            visible.push(seg)
+            data_index["synth"][seg] = counter
+            counter += 1
+        }
+        // TODO: annotations
+    }
+    for (let seg = 0; seg < nSegment; seg++) {
+        // Slider Steps
+        var visibility = Array.apply(null, Array(visible.length)).map(function (x: any, i: number) { return visible[i] == seg })
+        var step = {
+            label: `Segment ${seg}`,
+            method: "update",
+            args: [
+                { visible: visibility },
+                {
+                    title: `Segment ${seg}`,
+                    // annotations: annotations[i],
+                    xaxis: { range: sme.wran[seg] },
+                    yaxis: { autorange: true },
+                },
+            ],
+        }
+        steps.push(step)
+    }
+
+    var slider = {
+        pad: {
+            t: 30
+        },
+        active: 0,
+        steps: steps,
+    }
+
+    layout = {
+        xaxis: { title: "Wavelength [Å]" },
+        yaxis: { title: "Intensity" },
+        showlegend: true,
+        title: "Segment 0",
+        legend: { traceorder: "reversed" },
+        font: { family: "Open Sans, sans-serif" },
+        selectdirection: "h",
+        sliders: [slider],
+    }
+
+    var buttons = [["zoom2d", "pan2d", "select2d", "zoomIn2d", "zoomOut2d", "autoScale2d", "resetScale2d"]];
+    Plotly.react(graphDiv, data, layout, { responsive: true, modeBarButtons: buttons });
+
+    graphDiv.on('plotly_selected', (event: any) => {
+        var range: [number, number] = event.range.x
+        set_mask(sme, range)
+    })
+}
+
+var ButtonMaskLine = document.getElementById("btn-mask-line")
+ButtonMaskLine.addEventListener('click', () => {
+    mask_mode = "line"
+})
+
+
+var ButtonMaskCont = document.getElementById("btn-mask-cont")
+ButtonMaskCont.addEventListener('click', () => {
+    mask_mode = "cont"
+})
+
+
+var ButtonMaskBad = document.getElementById("btn-mask-bad")
+ButtonMaskBad.addEventListener('click', () => {
+    mask_mode = "bad"
+})
---
+++ src/renderer.ts
@@ -0,0 +1,317 @@
+// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+// All of the Node.js APIs are available in this process.
+
+const { dialog } = require('electron').remote
+var fs = require('fs');
+var JSZip = require("jszip");
+const { Table } = require("apache-arrow");
+
+interface ZipObject {
+    name: string
+    async: Function
+}
+
+interface SmeFile {
+    [id: string]: any;
+}
+
+var sme: SmeFile;
+
+function checkEndian() {
+    var arrayBuffer = new ArrayBuffer(2);
+    var uint8Array = new Uint8Array(arrayBuffer);
+    var uint16array = new Uint16Array(arrayBuffer);
+    uint8Array[0] = 0xAA; // set first byte
+    uint8Array[1] = 0xBB; // set second byte
+    if (uint16array[0] === 0xBBAA) return true; // little endian
+    if (uint16array[0] === 0xAABB) return false; // big endian
+    else throw new Error("Something crazy just happened");
+}
+
+function load_json(text: string) {
+    var content = JSON.parse(text)
+    return content
+}
+
+async function load_npz(data: ArrayBuffer) {
+    var zip = await JSZip.loadAsync(data)
+    var length = 0
+    zip.forEach(() => length += 1)
+    var arr = Array.from({ length: length }, (v, i) => null)
+    zip.forEach((relative_path: string, zipEntry: ZipObject) => {
+        arr[Number(zipEntry.name.slice(4, -4))] = zipEntry;
+    })
+
+    for (let index = 0; index < length; index++) {
+        const element: ZipObject = arr[index];
+        var tmp: ArrayBuffer = await element.async("arraybuffer")
+        arr[index] = load_npy(tmp)
+    }
+    return arr
+}
+
+function create_array(data: ArrayBuffer, descr: string) {
+    var endian = (descr[0] == "<")
+    var floatiness = descr[1]
+    var bytenumber = Number(descr[2])
+
+    if ((descr[0] != "|") && (endian != checkEndian())) {
+        // TODO: flip endianness of data
+        throw new Error(`Endianess of datafile ${descr[0]} does not match machine Endianness`)
+    }
+
+    // new DataView(buffer).setFloat64(0, endian_flag)
+    if (floatiness == "f") {
+        // Floating point number
+        if (bytenumber == 4) {
+            return new Float32Array(data)
+        }
+        if (bytenumber == 8) {
+            return new Float64Array(data)
+        }
+        throw new Error(`Array is floating point, but the bytenumber ${bytenumber} is not supported`)
+    }
+    if (floatiness == "i") {
+        // signed integer
+        if (bytenumber == 2) {
+            return new Int16Array(data)
+        }
+        if (bytenumber == 4) {
+            return new Int32Array(data)
+        }
+        if (bytenumber == 8) {
+            return new BigInt64Array(data)
+        }
+        throw new Error(`Array is integer, but the bytenumber ${bytenumber} is not supported`)
+    }
+    if (floatiness == "b") {
+        if (bytenumber == 1) {
+            return new Int8Array(data)
+        }
+        throw new Error(`Array is byte, but the bytenumber ${bytenumber} is not supported`)
+    }
+    throw new Error(`Datatype ${floatiness} of the Array not understood`)
+}
+
+function load_npy(data: ArrayBuffer) {
+    var decoder = new TextDecoder()
+    var magic = data.slice(0, 6)
+    var major = new DataView(data, 6, 1).getUint8(0)
+    var minor = new DataView(data, 7, 1).getUint8(0)
+    var header_len = new DataView(data, 8, 2).getUint16(0, true)
+
+    var header = decoder.decode(data.slice(10, 10 + header_len)).trim().slice(1, -1)
+
+    var descr = header.match("'descr':(.*?),")[1].trim().slice(1, -1)
+    var fortran = eval(header.match("'fortran_order':(.*?),")[1].trim().toLowerCase())
+    var shape: number[] = eval(header.match("'shape': *(\(.*\)),")[1].trim().replace("(", "[").replace(")", "]"))
+
+    var arraydata = create_array(data.slice(10 + header_len), descr)
+    //TODO: shape and fortran_order
+
+    if (shape.length > 1) {
+        if (shape.length == 2) {
+            var nseg = shape[0]
+            var npoints = shape[1]
+            var arr = Array.from({ length: nseg }, (v, i) => {
+                return arraydata.slice(i * npoints, (i + 1) * npoints)
+            })
+            return arr
+        }
+        console.warn("Only 1D and 2D arrays at the moment, but got shape: " + shape)
+
+    }
+
+    return arraydata
+}
+
+function load_feather(data: ArrayBuffer) {
+    console.warn("Linelist fileformat not working at the moment")
+    return data
+    // return Table.from([data]);
+}
+
+async function load_file(filename: string) {
+    var promise = new Promise((resolve, reject) => {
+        fs.readFile(filename, (err: any, data: any) => {
+            if (err) {
+                reject(err.message)
+                return;
+            }
+            resolve(data)
+        })
+    });
+    var data = await promise
+    var zip = await JSZip.loadAsync(data)
+
+    var length = 0
+    zip.forEach(() => length += 1)
+    var arr = Array.from({ length: length }, (v, i) => null)
+    var i = 0
+    zip.forEach((relative_path: string, zipEntry: ZipObject) => {
+        arr[i] = zipEntry;
+        i += 1
+    })
+
+    var files: SmeFile = {}
+
+    for (let index = 0; index < length; index++) {
+        const element: ZipObject = arr[index];
+        const name = element.name;
+        switch (name.split(".")[1]) {
+            case "json":
+                files[name.slice(0, -5)] = load_json(await element.async("string"))
+                break;
+            case "npz":
+                files[name.slice(0, -4)] = await load_npz(await element.async("arraybuffer"))
+                break;
+            case "npy":
+                files[name.slice(0, -4)] = load_npy(await element.async("arraybuffer"))
+                break;
+            case "feather":
+                files[name.slice(0, -8)] = load_feather(await element.async("arraybuffer"))
+                break;
+            default:
+                console.log("File type not understood: " + name)
+                break;
+        }
+    }
+    return files
+}
+
+function determine_datatype(obj: any) {
+    if (obj.constructor == Object) return "dict"
+    if (Array.isArray(obj)) return "array"
+    if (obj instanceof Float64Array) return "<f8"
+    if (obj instanceof Float32Array) return "<f4"
+    if (obj instanceof Int8Array) return "|b1"
+    if (obj instanceof Int16Array) return "<i2"
+    if (obj instanceof Int32Array) return "<i4"
+    if (obj instanceof BigInt64Array) return "<i8"
+    if (obj instanceof ArrayBuffer) return "feather"
+    throw new Error(`Data type of ${obj} not recognised`)
+}
+
+function save_npy(obj: Float64Array, dtype: string) {
+    var encoder = new TextEncoder()
+    var magic = new Uint8Array([147, 78, 85, 77, 80, 89]) // "\x93NUMPY"
+    var major = new Uint8Array([1])
+    var minor = new Uint8Array([0])
+
+    var shape = obj.length
+    var header_str = `{'descr': '${dtype}', 'fortran_order': False, 'shape': (${shape},), }`
+    var total = (6 + 2 + 2 + header_str.length + 1) % 64
+    if (total != 0) total = 64 - total
+    header_str = header_str.padEnd(header_str.length + total) + "\n"
+    var header = encoder.encode(header_str)
+    var header_len = new Uint8Array((new Uint16Array([header.length])).buffer)
+
+    var arraydata = new Uint8Array(obj.buffer)
+
+    // TODO: combine everything into one big buffer
+    var buffer = new Uint8Array(6 + 2 + 2 + header.length + obj.buffer.byteLength)
+    buffer.set(magic)
+    buffer.set(major, 6)
+    buffer.set(minor, 7)
+    buffer.set(header_len, 8)
+    buffer.set(header, 10)
+    buffer.set(arraydata, 10 + header.length)
+    return buffer
+}
+
+async function save_npz(obj: Array<Float64Array>) {
+    var zip = JSZip()
+    for (let i = 0; i < obj.length; i++) {
+        const element = obj[i];
+        var name = `arr_${i}.npy`
+        var dtype = determine_datatype(element)
+        var buffer = save_npy(element, dtype)
+        zip.file(name, buffer)
+    }
+    var stream = await zip.generateAsync({ type: "arraybuffer" })
+    return stream
+}
+
+function save_feather(obj: ArrayBuffer) {
+    return obj
+}
+
+async function save_file(fname: string, sme: SmeFile) {
+    var zip = JSZip()
+    console.log(sme)
+
+    for (const key in sme) {
+        if (sme.hasOwnProperty(key)) {
+            const element = sme[key];
+            var dtype = determine_datatype(element)
+            var content: any = ""
+            var ending = "dat"
+            switch (dtype) {
+                case "dict":
+                    content = JSON.stringify(element)
+                    ending = "json"
+                    break;
+                case "array":
+                    // Save as npz
+                    content = await save_npz(element)
+                    ending = "npz"
+                    break;
+                case "<f8":
+                case "<f4":
+                case "|b1":
+                case "<i2":
+                case "<i4":
+                case "<i8":
+                    // save as npy
+                    content = save_npy(element, dtype)
+                    ending = "npy"
+                    break;
+                case "feather":
+                    content = save_feather(element)
+                    ending = "feather"
+                    break;
+                default:
+                    throw new Error(`No saving mechanism for given filetype ${dtype}`)
+                    break;
+            }
+            zip.file(`${key}.${ending}`, content)
+        }
+    }
+    // var content: ArrayBuffer
+    // fs.writeFile(fname, content, (err: Error) => { if (err) throw err; console.log("Saved file") })
+    zip
+        .generateNodeStream({ type: 'nodebuffer', streamFiles: true })
+        .pipe(fs.createWriteStream(fname))
+        .on('finish', function () {
+            // JSZip generates a readable stream with a "end" event,
+            // but is piped here in a writable stream which emits a "finish" event.
+            console.log("out.zip written.");
+        });
+}
+
+
+var ButtonLoad = document.getElementById("btn-load")
+ButtonLoad.addEventListener('click', async (event) => {
+    var out = await dialog.showOpenDialog({ properties: ["openFile"] })
+    if (!out.canceled) {
+        var fname = out.filePaths[0];
+        console.log("Opening new file: " + fname)
+        sme = await load_file(fname)
+        plot_sme(sme)
+    } else {
+        console.log("User did not select a file")
+    }
+})
+
+var ButtonSave = document.getElementById("btn-save")
+ButtonSave.addEventListener('click', async (event) => {
+    var out = await dialog.showSaveDialog({ properties: ["showOverwriteConfirmation"] })
+    if (!out.canceled) {
+        var fname = out.filePath
+        console.log("Saving to file: " + fname)
+        save_file(fname, sme)
+    } else {
+        console.log("User did not select a file")
+    }
+})
