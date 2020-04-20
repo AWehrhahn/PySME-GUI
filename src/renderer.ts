3fb9185413e1eccd7b6e81d77877ee8d45b572da
Ansgar Wehrhahn <ansgar.wehrhahn@physics.uu.se>
2020-04-20 13:56:55 +0200

    More fields

--- src/renderer.ts
+++ src/renderer.ts
@@ -424,8 +424,35 @@ async function load_parameter_values(sme: SmeFile) {
     //TODO: do this for every field
     var FieldTeff: any = document.getElementById("par-teff")
     var FieldLogg: any = document.getElementById("par-logg")
+    var FieldMonh: any = document.getElementById("par-monh")
+    var FieldVmic: any = document.getElementById("par-vmic")
+    var FieldVmac: any = document.getElementById("par-vmac")
+    var FieldVsini: any = document.getElementById("par-vsini")
+    var FieldGamma6: any = document.getElementById("par-gamma6")
+
+
+    var FieldCscaleFlag: any = document.getElementById("par-cscale-flag")
+    var FieldCscaleType: any = document.getElementById("par-cscale-type")
+    var FieldVradFlag: any = document.getElementById("par-vrad-flag")
+
+    var FieldFitparameters: any = document.getElementById("par-fitparameters")
+
     FieldTeff.value = sme.teff
     FieldLogg.value = sme.logg
+    FieldMonh.value = sme["abund/info"]["monh"]
+    FieldVmic.value = sme.vmic
+    FieldVmac.value = sme.vmac
+    FieldVsini.value = sme.vsini
+    FieldGamma6.value = sme.gam6
+
+    FieldCscaleFlag.value = sme.cscale_flag
+    FieldCscaleType.value = sme.cscale_type
+    FieldVradFlag.value = sme.vrad_flag
+
+    // Fitparameters are comma seperated text
+    // TODO: is there a better option?
+    FieldFitparameters.value = sme.fitparameters.join(", ")
+
 }
 
 var ButtonLoad = document.getElementById("btn-load")
