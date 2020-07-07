ed7b65bb94c1979dbe96f88bc379e6b53710b287
Ansgar Wehrhahn <ansgar.wehrhahn@physics.uu.se>
2020-07-07 14:52:07 +0200

    Update Python command option

--- src/python.ts
+++ src/python.ts
@@ -1,9 +1,12 @@
+// Make this a setting that persist through restart?
+var python_command: string = "python"
 
 async function call_python(script: string, args: string[]) {
     var promise = new Promise<boolean>((resolve, reject) => {
         var script_file = join(__dirname, "scripts", script)
         args.unshift(script_file)
-        var child = spawn("python", args)
+        // TODO test the version of the default python and also try python3
+        var child = spawn(python_command, args)
 
         child.on('exit', (code: any, signal: any) => {
             console.log(`child process exited with code ${code}`);
